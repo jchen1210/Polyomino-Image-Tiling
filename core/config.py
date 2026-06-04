d4aaed7b3ab6e024682d018234b2788c358f0310
Jeremy <jeremychen3141@icloud.com>
2026-06-03 22:03:18 -0700

    refactor config to use @dataclass methods

--- core/config.py
+++ core/config.py
@@ -5,16 +5,15 @@ class OptimizationSettings:
     '''
     Hyperparameters for the CVXPY model
     '''
-    def __init__(self, edge_penalty_weight: float, size_bonus_weight: float):
-        self.edge_penalty_weight = edge_penalty_weight
-        self.size_bonus_weight = size_bonus_weight
+    edge_penalty_weight: float
+    size_bonus_weight: float
+    tolerance: float = 0
 
 @dataclass
 class ImageSettings:
     '''
     Settings for the image tiling
     '''
-    def __init__(self, num_rows: int, num_cols: int, block_size: int):
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.block_size = block_size
+    num_rows: int
+    num_cols: int
+    block_size: int
