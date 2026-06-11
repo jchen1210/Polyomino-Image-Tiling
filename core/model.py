fixed polyomino rotation bug

core/model.py
@@ -46,6 +46,9 @@ class TilingOptimizer:
         presolve = self.settings.presolve
         threshold = self.settings.presolve_threshold
 
+        if presolve:
+            print("Presolving model...")
+
         for (tile, (i, j)) in raw_placements:
             footprint_mask = tile.footprint_mask
             normalized_tile = np.array(tile.colour) / 255.0
@@ -96,7 +99,7 @@ class TilingOptimizer:
         tol = self.settings.tolerance
 
         print("Solving...")
-        problem.solve(verbose=True, solver='HIGHS',
+        problem.solve(verbose=False, solver='HIGHS',
             highs_options={
                 "mip_rel_gap": tol,
                 })

core/tiles.py
@@ -19,7 +19,7 @@ class Polyomino:
         seen_shapes = set()
         for k in range(4):
             rotated_mask = np.rot90(self.footprint_mask, k).copy()
-            shape_sig = rotated_mask.tobytes()
+            shape_sig = tuple(map(tuple, rotated_mask))
 
             if shape_sig not in seen_shapes:
                 seen_shapes.add(shape_sig)