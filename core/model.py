4e031f9202eccde3125df1b4f256c67f413a6743
Jeremy <jeremychen3141@icloud.com>
2026-06-25 17:43:50 -0700

    rewrote prepare() as an orchestrator, added setup timing

--- core/model.py
+++ core/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cvxpy as cp
+import time
 from collections import defaultdict
 from dataclasses import dataclass
 from typing import Optional
@@ -23,30 +24,101 @@ class TilingOptimizer:
     def __init__(self, image_data: ImageData, tile_config: TileConfig, settings: OptimizationSettings):
         self.image_data = image_data
         self.settings = settings
-        self._x = None
-        self.costs = []
-        self.placements = []
-        self.num_placements = 0
-        self.block_to_placements = defaultdict(list)
-
         self.tile_set = TileSet(tile_config.shapes, tile_config.scales, image_data.palette)
 
-    def prepare(self):
-        raw_placements, raw_num_placements = self.tile_set.generate_placements(
-            self.image_data.num_cols,
-            self.image_data.num_rows
-        )
-        print(f"Your problem has {raw_num_placements} possible tile placements")
-        s = self.settings
-        edge_weight = s.edge_penalty_weight
-        size_weight = s.size_bonus_weight
-        edge_grid = self.image_data.laplacian_grid
-        rgb_grid = self.image_data.rgb_grid / 255.0     # normalizing to range [0, 1]
-        presolve = s.presolve
-        threshold = s.presolve_threshold
+        self._x = None
+        self.costs = None
+        self.placements = None
+        self.block_to_placements = None        
 
-        if presolve:
-            print("Presolving model...")
+    def prepare(self):
+        t0 = time.monotonic()
+        print("Setting up problem...")
+        raw_placements, raw_num_placements, raw_costs = self._build_costs()
+
+        warm_start = None
+        if self.settings.presolve:
+            self.placements, num_placements, self.costs, warm_start = self.presolve(raw_placements, raw_costs)
+        else:
+            self.placements, num_placements, self.costs = raw_placements, raw_num_placements, raw_costs
+
+        t1 = time.monotonic()
+        self.block_to_placements = self._build_block_mapping(self.placements)
+        self._x = cp.Variable(num_placements, boolean=True)
+        if warm_start is not None:
+            self._x.value = warm_start
+        
+        constraints = self._build_constraints()
+        objective = self._build_objective()
+
+        print(f"    Model built ({time.monotonic()-t1:.2f}s)")
+        print(f"Total setup: {time.monotonic()-t0:.2f}s")
+        return cp.Problem(objective, constraints)
+
+    def presolve(self, placements, costs):
+        t0 = time.monotonic()
+
+        pruned_placements, pruned_num_placements, pruned_costs = self._prune_placements(placements, costs)
+        warm_start = self._warm_start(pruned_placements, pruned_costs)
+
+        print(f"    Presolve: {pruned_num_placements} retained, {len(placements)-pruned_num_placements} pruned ({time.monotonic()-t0:.2f}s)")
+        return pruned_placements, pruned_num_placements, pruned_costs, warm_start
+    
+    def solve(self) -> SolverResult:
+        problem = self.prepare()
+
+        assert self._x is not None
+        tol = self.settings.tolerance
+
+        print("Solving...")
+        problem.solve(verbose=True, solver='HIGHS', warm_start=True,
+            highs_options={
+                "mip_rel_gap": tol,
+                })
+        
+        if problem.status != 'optimal':
+            return SolverResult(False, problem.status)
+        else:
+            return SolverResult(True, problem.status, self._x.value, self.placements)
+
+    def _warm_start(self, placements, costs):
+        return np.zeros(len(placements))
+
+    def _build_block_mapping(self, placements):
+        block_to_placements = defaultdict(list)
+        for i, (tile, anchor) in enumerate(placements):
+            for coord in tile.anchor_footprint(anchor):
+                block_to_placements[coord].append(i)
+        return block_to_placements
+
+    def _prune_placements(self, placements, costs):
+        pruned_placements = []
+        pruned_costs = []
+        pruned_num_placements = 0
+
+        for (tile, anchor), cost in zip(placements, costs):
+            mean_cost = cost / np.sum(tile.footprint_mask)
+            if mean_cost > self.settings.presolve_threshold:
+                continue
+            pruned_placements.append((tile, anchor))
+            pruned_costs.append(cost)
+            pruned_num_placements += 1
+
+        return pruned_placements, pruned_num_placements, np.array(pruned_costs)
+
+    def _build_costs(self):
+        t0 = time.monotonic()
+        s, img = self.settings, self.image_data
+        raw_placements, raw_num_placements = self.tile_set.generate_placements(
+            img.num_cols,
+            img.num_rows
+        )
+        print(f"    Placements generated: {raw_num_placements} ({time.monotonic()-t0:.2f}s)")
+        
+        t0 = time.monotonic()
+        edge_weight, size_weight = s.edge_penalty_weight, s.size_bonus_weight
+        edge_grid, rgb_grid = img.laplacian_grid, img.rgb_grid / 255.0     # normalizing rgb grid to range [0, 1]
+        raw_costs = []
 
         for (tile, (i, j)) in raw_placements:
             footprint_mask = tile.footprint_mask
@@ -63,47 +135,20 @@ class TilingOptimizer:
             size_bonus = -size_weight * (tile.scale - 1)**2
             tile_cost = rgb_error + edge_pen + size_bonus
 
-            if presolve and (tile_cost / np.sum(footprint_mask)) > threshold:
-                continue
+            raw_costs.append(tile_cost)
 
-            self.costs.append(tile_cost)
-            self.placements.append((tile, (i, j)))
-            for coord in tile.anchor_footprint((i, j)):
-                self.block_to_placements[coord].append(self.num_placements)
-            self.num_placements += 1
-
-        self._x = cp.Variable(self.num_placements, boolean=True)
-        if presolve:
-            print(f"Presolve reduction: placements {self.num_placements}(-{raw_num_placements-self.num_placements})")
+        print(f"    Costs computed ({time.monotonic()-t0:.2f}s)")
+        return raw_placements, raw_num_placements, np.array(raw_costs)
 
     def _build_objective(self) -> cp.Minimize:
-        return cp.Minimize(cp.sum(cp.multiply(self.costs, self._x)))
+        assert self.costs is not None
+        return cp.Minimize(self.costs @ self._x)
     
     def _build_constraints(self) -> list[cp.Constraint]:
         assert self._x is not None
+        assert self.block_to_placements is not None
         constraints = []
         for coord, placement_indices in self.block_to_placements.items():
             constraints.append(cp.sum(self._x[placement_indices]) == 1)
         
         return constraints
-
-    def solve(self) -> SolverResult:
-        if self._x is None:
-            self.prepare()
-            assert self._x is not None
-
-        constraints = self._build_constraints()
-        objective = self._build_objective()
-        problem = cp.Problem(objective, constraints)
-        tol = self.settings.tolerance
-
-        print("Solving...")
-        problem.solve(verbose=True, solver='HIGHS',
-            highs_options={
-                "mip_rel_gap": tol,
-                })
-        
-        if problem.status != 'optimal':
-            return SolverResult(False, problem.status)
-        else:
-            return SolverResult(True, problem.status, self._x.value, self.placements)
